fd5b7a45ddfef78f05d9ea231090a5790285d480
Felipe K. Lisak <felipeklisak@gmail.com>
2024-12-22 11:44:34 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Fe on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
